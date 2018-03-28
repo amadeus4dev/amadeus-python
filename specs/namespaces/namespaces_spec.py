82b55c0178bba1d140c78fbc9241ca11ed3ea37b
Cristiano Betta <cristiano@betta.io>
2018-03-28 22:33:46 +0100

    Make tests and linting pass

--- specs/namespaces/namespaces_spec.py
+++ specs/namespaces/namespaces_spec.py
@@ -1,4 +1,4 @@
-from mamba import description, it
+from mamba import description, it, before, context
 from expects import expect, be_none
 from doublex import method_returning
 from doublex_expects import have_been_called_with
@@ -13,69 +13,70 @@ with description('Namespaces') as self:
         )
 
     with it('should define all expected paths'):
-        expect(self.client.reference_data).not_to(be_none);
-        expect(self.client.reference_data.urls).not_to(be_none);
-        expect(self.client.reference_data.urls.checkin_links).not_to(be_none);
-        expect(self.client.reference_data.location).not_to(be_none);
-        expect(self.client.reference_data.locations).not_to(be_none);
-        expect(self.client.reference_data.locations.airports).not_to(be_none);
+        expect(self.client.reference_data).not_to(be_none)
+        expect(self.client.reference_data.urls).not_to(be_none)
+        expect(self.client.reference_data.urls.checkin_links).not_to(be_none)
+        expect(self.client.reference_data.location).not_to(be_none)
+        expect(self.client.reference_data.locations).not_to(be_none)
+        expect(self.client.reference_data.locations.airports).not_to(be_none)
 
-        # expect(self.client.travel).not_to(be_none);
-        # expect(self.client.travel.analytics).not_to(be_none);
-        # expect(self.client.travel.analytics.air_traffics).not_to(be_none);
-        # expect(self.client.travel.analytics.fare_searches).not_to(be_none);
+        # expect(self.client.travel).not_to(be_none)
+        # expect(self.client.travel.analytics).not_to(be_none)
+        # expect(self.client.travel.analytics.air_traffics).not_to(be_none)
+        # expect(self.client.travel.analytics.fare_searches).not_to(be_none)
         #
-        # expect(self.client.shopping).not_to(be_none);
-        # expect(self.client.shopping.flight_dates).not_to(be_none);
-        # expect(self.client.shopping.flight_destinations).not_to(be_none);
-        # expect(self.client.shopping.flight_offers).not_to(be_none);
+        # expect(self.client.shopping).not_to(be_none)
+        # expect(self.client.shopping.flight_dates).not_to(be_none)
+        # expect(self.client.shopping.flight_destinations).not_to(be_none)
+        # expect(self.client.shopping.flight_offers).not_to(be_none)
         #
-        # expect(self.client.shopping.hotel_offers).not_to(be_none);
-        # expect(self.client.shopping.hotel).not_to(be_none);
-        # expect(self.client.shopping.hotel(123).offer).not_to(be_none);
-        # expect(self.client.shopping.hotel(123).hotel_offers).not_to(be_none);
+        # expect(self.client.shopping.hotel_offers).not_to(be_none)
+        # expect(self.client.shopping.hotel).not_to(be_none)
+        # expect(self.client.shopping.hotel(123).offer).not_to(be_none)
+        # expect(self.client.shopping.hotel(123).hotel_offers).not_to(be_none)
 
     with it('should define all expected .get methods'):
-        expect(self.client.reference_data.urls.checkin_links.get).not_to(be_none);
-        expect(self.client.reference_data.location('ALHR').get).not_to(be_none);
-        expect(self.client.reference_data.locations.get).not_to(be_none);
-        expect(self.client.reference_data.locations.airports.get).not_to(be_none);
+        client = self.client
+        expect(client.reference_data.urls.checkin_links.get).not_to(be_none)
+        expect(client.reference_data.location('ALHR').get).not_to(be_none)
+        expect(client.reference_data.locations.get).not_to(be_none)
+        expect(client.reference_data.locations.airports.get).not_to(be_none)
 
-        # expect(self.client.travel.analytics.airTraffics.get).toBeDefined();
-        # expect(self.client.travel.analytics.fareSearches.get).toBeDefined();
+        # expect(self.client.travel.analytics.airTraffics.get).toBeDefined()
+        # expect(self.client.travel.analytics.fareSearches.get).toBeDefined()
         #
-        # expect(self.client.shopping.flightDates.get).toBeDefined();
-        # expect(self.client.shopping.flightDestinations.get).toBeDefined();
-        # expect(self.client.shopping.flightOffers.get).toBeDefined();
+        # expect(self.client.shopping.flightDates.get).toBeDefined()
+        # expect(self.client.shopping.flightDestinations.get).toBeDefined()
+        # expect(self.client.shopping.flightOffers.get).toBeDefined()
         #
-        # expect(self.client.shopping.hotelOffers.get).toBeDefined();
-        # expect(self.client.shopping.hotel(123).hotelOffers.get).toBeDefined();
-        # expect(self.client.shopping.hotel(123).offer(234).get).toBeDefined();
+        # expect(self.client.shopping.hotelOffers.get).toBeDefined()
+        # expect(self.client.shopping.hotel(123).hotelOffers.get).toBeDefined()
+        # expect(self.client.shopping.hotel(123).offer(234).get).toBeDefined()
 
     with context('testing all calls to the client'):
         with before.each:
             self.client.get = method_returning(None)
 
         with it('.amadeus.reference_data.urls.checkin_links.get'):
-            self.client.reference_data.urls.checkin_links.get(a='b');
+            self.client.reference_data.urls.checkin_links.get(a='b')
             expect(self.client.get).to(have_been_called_with(
                 '/v2/reference-data/urls/checkin-links', a='b'
             ))
 
         with it('.amadeus.reference_data.location().get'):
-            self.client.reference_data.location('ALHR').get(a='b');
+            self.client.reference_data.location('ALHR').get(a='b')
             expect(self.client.get).to(have_been_called_with(
                 '/v1/reference-data/locations/ALHR', a='b'
             ))
 
         with it('.amadeus.reference_data.locations.get'):
-            self.client.reference_data.locations.get(a='b');
+            self.client.reference_data.locations.get(a='b')
             expect(self.client.get).to(have_been_called_with(
                 '/v1/reference-data/locations', a='b'
             ))
 
         with it('.amadeus.reference_data.locations.airports.get'):
-            self.client.reference_data.locations.airports.get(a='b');
+            self.client.reference_data.locations.airports.get(a='b')
             expect(self.client.get).to(have_been_called_with(
                 '/v1/reference-data/locations/airports', a='b'
             ))
