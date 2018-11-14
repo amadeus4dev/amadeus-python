d69e98a402264e21060e19dc646535d1cb75a985
Anthony Roux <anthony.roux@amadeus.com>
2018-11-14 15:43:17 +0100

    Redesign of the Flight Most Searched Destinations API

--- amadeus/travel/_analytics.py
+++ amadeus/travel/_analytics.py
@@ -1,10 +1,9 @@
 from amadeus.client.decorator import Decorator
 from .analytics._air_traffic import AirTraffic
-from .analytics._fare_searches import FareSearches
+
 
 
 class Analytics(Decorator, object):
     def __init__(self, client):
         Decorator.__init__(self, client)
-        self.fare_searches = FareSearches(client)
         self.air_traffic = AirTraffic(client)
