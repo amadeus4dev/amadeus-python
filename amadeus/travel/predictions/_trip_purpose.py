1487c303939c3a2833a92e8d8f10257dc97acac6
Anna Tsolakou <tsolakou.a@gmail.com>
2019-09-27 22:03:39 +0200

    update documentation on trip prediction

--- amadeus/travel/predictions/_trip_purpose.py
+++ amadeus/travel/predictions/_trip_purpose.py
@@ -4,31 +4,32 @@ from amadeus.client.decorator import Decorator
 class TripPurpose(Decorator, object):
     def get(self, **params):
         '''
-        Get forecast traveler purpose, Business or Leisure,
-        together with the probability in the context of search & shopping
+        Predicts traveler purpose, Business or Leisure,
+        with the probability in the context of search & shopping
 
         .. code-block:: python
 
-            amadeus.trip.trip_purpose_prediction.get(originLocationCode='XKPARC12',
-            destinationLocationCode='XKPARC12',
-            departureDate='XKPARC12', returnDate='XKPARC12',
-            searchDate='XKPARC12')
+            amadeus.travel.predictions.trip_purpose.get(
+                            originLocationCode='NYC',
+                            destinationLocationCode='MAD',
+                            departureDate='2020-08-01',
+                            returnDate='2020-08-12',
+                            searchDate='2020-06-11')
 
-        :param originLocationCode: Amadeus Property Code (8 chars), for
-            example ``XKPARC12``.
+        :param originLocationCode: the City/Airport IATA code from which
+            the flight will depart. ``"NYC"``, for example for New York
 
-        :param destinationLocationCode: the City/Airport IATA code from which
-        the flight will
-            depart. ``"MAD"``, for example for Madrid.
+        :param destinationLocationCode: the City/Airport IATA code to which
+            the flight is going. ``"MAD"``, for example for Madrid
 
-        :param departureDate: the City/Airport IATA code to which the flight is
-            going. ``"BOS"``, for example for Boston.
+        :param departureDate: the date on which to fly out, in `YYYY-MM-DD` format
 
-        :param returnDate: the date on which to fly out, in `YYYY-MM-DD`
-            format
+        :param returnDate: the date on which the flight returns to the origin,
+            in `YYYY-MM-DD` format
 
-        :param searchDate: the date on which to fly out, in `YYYY-MM-DD`
-            format
+        :param searchDate: the date on which the traveler performs the search,
+            in `YYYY-MM-DD` format.
+            If it is not specified the current date will be used
 
         :rtype: amadeus.Response
         :raises amadeus.ResponseError: if the request could not be completed
