2b2e3855bd5996acb6cec428591d180bee02e015
Anna Tsolakou <tsolakou.a@gmail.com>
2019-12-14 00:13:53 +0100

    update tests description

--- specs/mixins/encoder_spec.py
+++ specs/mixins/encoder_spec.py
@@ -4,15 +4,15 @@ from expects import expect, equal
 from amadeus.mixins.encoder import encode_file_to_base64
 from amadeus.travel._encoder import from_file, from_base64
 
-with description('File Encoding'):
-    with it('should be encoded to base64'):
+with description('Mixins/Encoding'):
+    with it('file should be encoded to base64'):
         file = str(os.path.dirname(os.path.abspath(__file__))) \
                + '/encoder_specs_file.eml'
         base64 = 'Ym9va2luZw=='
         expect(encode_file_to_base64(file)).to(equal(base64))
 
-with description('File to POST body'):
-    with it('should generate the defined POST body'):
+with description('Travel/From File'):
+    with it('should generate the defined POST body from file'):
         file = str(os.path.dirname(os.path.abspath(__file__))) \
                + '/encoder_specs_file.eml'
         base64 = 'Ym9va2luZw=='
@@ -24,8 +24,8 @@ with description('File to POST body'):
         }
         expect(from_file(file)).to(equal(body))
 
-with description('Base64 ti POST body'):
-    with it('should generate the defined POST body'):
+with description('Travel/From Base64'):
+    with it('should generate the defined POST body from base64'):
         base64 = 'Ym9va2luZw=='
         body = {
             'data': {
