665aa20818434f4452f8a299c31d89717a8d6112
Anna Tsolakou <tsolakou.a@gmail.com>
2019-12-19 10:58:56 +0100

    add patch with name update

--- amadeus/travel/_encoder.py
+++ amadeus/travel/_encoder.py
@@ -4,16 +4,16 @@ from amadeus.mixins.encoder import encode_file_to_base64
 # Encodes file to Base64 and constructs POST body
 def from_file(file):
     encoded_file = encode_file_to_base64(file)
-    return construct_post_body(encoded_file)
+    return __build_trip_parser_body(encoded_file)
 
 
 # Constructs POST body from Base64
 def from_base64(base64):
-    return construct_post_body(base64)
+    return __build_trip_parser_body(base64)
 
 
 # Takes a Base64 and returns a dict
-def construct_post_body(base64):
+def __build_trip_parser_body(base64):
     return {
         'data': {
             'type': 'trip-parser-job',
