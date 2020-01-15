ee5cb5f3c8a87cd71b984f13ec8689bf39d6cd5c
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-15 15:34:54 +0100

    choose five examples to run

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,4 +1,10 @@
-if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $TRAVIS_PULL_REQUEST_BRANCH != "master" ]] ; then python samples/flight_choice_prediction/flight_choice_prediction.py ; fi
+if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $TRAVIS_PULL_REQUEST_BRANCH != "master" ]] ; then
+  python samples/ai_generated_photos/ai_generated_photos.py &
+  python samples/flight_choice_prediction/flight_choice_prediction.py &
+  python samples/flight_offers_search/flight_offers_search.py &
+  python samples/hotel_ratings/hotel_ratings.py &
+  python samples/points_of_interest/points_of_interest.py &
+fi
 if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
   for folder in samples/*/ ; do
     for file in "$folder"/* ; do
