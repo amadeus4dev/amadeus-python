c291b401509f123b9f2408efc788d072e7df5b38
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-15 15:21:17 +0100

    update travis

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,8 +1,11 @@
-for folder in samples/*/ ; do
-	for file in "$folder"/* ; do
-		if [[ $file == *.py ]]
-		then
-			python "$file"
-		fi
-	done
-done
+if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $TRAVIS_PULL_REQUEST_BRANCH != "master" ]] ; then python samples/flight_choice_prediction/flight_choice_prediction.py ; fi
+if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
+  for folder in samples/*/ ; do
+    for file in "$folder"/* ; do
+      if [[ $file == *.py ]]
+      then
+        python "$file"
+      fi
+    done
+  done
+; fi
