d8e75455dce0487dab5880c14231203ff7c846c1
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-21 15:42:00 +0100

    test all files

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,13 +1,13 @@
 if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $TRAVIS_PULL_REQUEST_BRANCH != "master" ]] ; then
-  python examples/hotel_search/hotel_search.py &
-  PID=$! && wait $PID && sleep 0.5 && echo "done"
-  python examples/flight_choice_prediction/flight_choice_prediction.py &
-  PID=$! && wait $PID && sleep 0.5 && echo "done"
-  python examples/flight_offers_search/flight_offers_search.py &
-  PID=$! && wait $PID && sleep 0.5 && echo "done"
-  python examples/hotel_ratings/hotel_ratings.py &
-  PID=$! && wait $PID && sleep 0.5 && echo "done"
-  python examples/points_of_interest/points_of_interest.py
+  for folder in examples/*/ ; do
+    for file in "$folder"/* ; do
+      if [[ $file == *.py ]]
+      then
+        python "$file"
+        PID=$! && wait $PID && sleep 0.5
+      fi
+    done
+  done
 fi
 if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
   for folder in examples/*/ ; do
@@ -15,6 +15,7 @@ if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
       if [[ $file == *.py ]]
       then
         python "$file"
+        PID=$! && wait $PID && sleep 0.5
       fi
     done
   done
