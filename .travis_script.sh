3d75ef48f56e386a1372613a5eda8ebc74f56cc6
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-21 16:00:48 +0100

    revert sleep time

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,13 +1,13 @@
 if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $TRAVIS_PULL_REQUEST_BRANCH != "master" ]] ; then
-  for folder in examples/*/ ; do
-    for file in "$folder"/* ; do
-      if [[ $file == *.py ]]
-      then
-        python "$file"
-        PID=$! && wait $PID && sleep 1 && echo "done"
-      fi
-    done
-  done
+  python examples/hotel_search/hotel_search.py &
+  PID=$! && wait $PID && sleep 1
+  python examples/flight_choice_prediction/flight_choice_prediction.py &
+  PID=$! && wait $PID && sleep 1
+  python examples/flight_offers_search/flight_offers_search.py &
+  PID=$! && wait $PID && sleep 1
+  python examples/hotel_ratings/hotel_ratings.py &
+  PID=$! && wait $PID && sleep 1
+  python examples/points_of_interest/points_of_interest.py
 fi
 if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
   for folder in examples/*/ ; do
@@ -15,7 +15,7 @@ if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
       if [[ $file == *.py ]]
       then
         python "$file"
-        PID=$! && wait $PID && sleep 0.5
+        PID=$! && wait $PID && sleep 1
       fi
     done
   done
