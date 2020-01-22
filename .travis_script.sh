1876096ed692b7dbb1a0f7e4073bcb716ea71348
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-22 11:04:55 +0100

    revert examples to defined rules

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,21 +1,18 @@
 if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $TRAVIS_PULL_REQUEST_BRANCH != "master" ]] ; then
-  for folder in examples/*/ ; do
-    for file in "$folder"/* ; do
-      if [[ $file == *.py ]]
-      then
-        python "$file" && echo "done"
-      fi
-    done
-  done
+python examples/hotel_search/hotel_search.py &
+python examples/flight_choice_prediction/flight_choice_prediction.py &
+python examples/flight_offers_search/flight_offers_search.py &
+python examples/hotel_ratings/hotel_ratings.py &
+python examples/points_of_interest/points_of_interest.py
 fi
 if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
-  for folder in examples/*/ ; do
-    for file in "$folder"/* ; do
-      if [[ $file == *.py ]]
-      then
-        python "$file"
-        PID=$! && wait $PID && sleep 1
-      fi
-    done
-  done
+for folder in examples/*/ ; do
+for file in "$folder"/* ; do
+if [[ $file == *.py ]]
+then
+python "$file"
+PID=$! && wait $PID && sleep 1
+fi
+done
+done
 fi
