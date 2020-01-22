ef458878b079d626663c7e5813063d5003a29e24
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-22 10:27:57 +0100

    test

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,30 +1,21 @@
-if [[ $TRAVIS_PYTHON_VERSION == '3.6.3' ]]; then
-  if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
-    for folder in examples/*/ ; do
-      for file in "$folder"/* ; do
-        if [[ $file == *.py ]]
-        then
-          python "$file" && echo "done"
-        fi
-      done
+if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $TRAVIS_PULL_REQUEST_BRANCH != "master" ]] ; then
+  for folder in examples/*/ ; do
+    for file in "$folder"/* ; do
+      if [[ $file == *.py ]]
+      then
+        python "$file" && echo "done"
+      fi
     done
-  fi
-  if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
-    for folder in examples/*/ ; do
-      for file in "$folder"/* ; do
-        if [[ $file == *.py ]]
-        then
-          python "$file"
-        fi
-      done
+  done
+fi
+if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
+  for folder in examples/*/ ; do
+    for file in "$folder"/* ; do
+      if [[ $file == *.py ]]
+      then
+        python "$file"
+        PID=$! && wait $PID && sleep 1
+      fi
     done
-  fi
-fi
-
-if [[ $TRAVIS_PYTHON_VERSION == '3.6.3' ]]; then
-echo "proto_sosto"
-fi
-
-if [[ $TRAVIS_PYTHON_VERSION == 'py36' ]]; then
-echo "deutero_sosto"
+  done
 fi
