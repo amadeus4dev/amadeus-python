f33f399b76d35dfba86e309a58e5ce8537f1f736
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-22 19:26:04 +0100

    print error msg

--- examples/flight_offers_search/flight_offers_search.py
+++ examples/flight_offers_search/flight_offers_search.py
@@ -9,4 +9,5 @@ try:
     response = amadeus.shopping.flight_offers_search.get(originLocationCode='SYAAD', destinationLocationCode='BKK', departureDate='2020-07-01', adults=1)
     # print(response.data)
 except ResponseError as error:
+    print(error)
     sys.exit(1)
