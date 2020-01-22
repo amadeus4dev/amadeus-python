c892165ef9ba5fa2bc0ece333a69bbba263441e2
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-01-22 09:47:56 +0100

    run examples only for python 3.6.3

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,21 +1,24 @@
-if [[ $TRAVIS_EVENT_TYPE == "pull_request" && $TRAVIS_PULL_REQUEST_BRANCH != "master" ]] ; then
-  for folder in examples/*/ ; do
-    for file in "$folder"/* ; do
-      if [[ $file == *.py ]]
-      then
-        python "$file"
-      fi
+if [[ $(python --version 2>&1) == *3\.6\.3 ]]; then
+  if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
+    for folder in examples/*/ ; do
+      for file in "$folder"/* ; do
+        if [[ $file == *.py ]]
+        then
+          python "$file"
+          PID=$! && wait $PID && sleep 1
+        fi
+      done
     done
-  done
-fi
-if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
-  for folder in examples/*/ ; do
-    for file in "$folder"/* ; do
-      if [[ $file == *.py ]]
-      then
-        python "$file"
-        PID=$! && wait $PID && sleep 1
-      fi
+  fi
+  if [[ $TRAVIS_EVENT_TYPE == "push" && $TRAVIS_BRANCH == "master" ]] ; then
+    for folder in examples/*/ ; do
+      for file in "$folder"/* ; do
+        if [[ $file == *.py ]]
+        then
+          python "$file"
+          PID=$! && wait $PID && sleep 1
+        fi
+      done
     done
-  done
+  fi
 fi
