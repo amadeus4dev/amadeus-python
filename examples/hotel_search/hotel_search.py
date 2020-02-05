5478013a8f33e6a092224351f97129abb1306135
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-02-05 18:14:35 +0100

    add typerror to exception

--- examples/hotel_search/hotel_search.py
+++ examples/hotel_search/hotel_search.py
@@ -17,5 +17,5 @@ try:
     payments = { 'id': 1, 'method': 'creditCard', 'card': { 'vendorCode': 'VI', 'cardNumber': '4151289722471370', 'expiryDate': '2021-08' } }
     hotel_booking = amadeus.booking.hotel_bookings.post(offer, guests, payments)
     # print(hotel_booking.data)
-except ResponseError:
+except (TypeError, ResponseError):
     pass
