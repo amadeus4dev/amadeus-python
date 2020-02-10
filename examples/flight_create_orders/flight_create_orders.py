05191f25c51f201bf80dfe35cc02874a05967ec4
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-02-10 12:30:19 +0100

    change traveler format

--- examples/flight_create_orders/flight_create_orders.py
+++ examples/flight_create_orders/flight_create_orders.py
@@ -2,35 +2,7 @@ from amadeus import Client, ResponseError
 
 amadeus = Client()
 
-traveler = {
-  'id': '1',
-  'dateOfBirth': '1982-01-16',
-  'name': {
-    'firstName': 'JORGE',
-    'lastName': 'GONZALES'
-  },
-  'gender': 'MALE',
-  'contact': {
-    'emailAddress': 'jorge.gonzales833@telefonica.es',
-    'phones': [{
-      'deviceType': 'MOBILE',
-      'countryCallingCode': '34',
-      'number': '480080076'
-    }]
-  },
-  'documents': [{
-    'documentType': 'PASSPORT',
-    'birthPlace': 'Madrid',
-    'issuanceLocation': 'Madrid',
-    'issuanceDate': '2015-04-14',
-    'number': '00000000',
-    'expiryDate': '2025-04-14',
-    'issuanceCountry': 'ES',
-    'validityCountry': 'ES',
-    'nationality': 'ES',
-    'holder': True
-  }]
-}
+traveler = { 'id': '1', 'dateOfBirth': '1982-01-16', 'name': { 'firstName': 'JORGE', 'lastName': 'GONZALES' }, 'gender': 'MALE', 'contact': { 'emailAddress': 'jorge.gonzales833@telefonica.es', 'phones': [ { 'deviceType': 'MOBILE', 'countryCallingCode': '34', 'number': '480080076' } ] }, 'documents': [ { 'documentType': 'PASSPORT', 'birthPlace': 'Madrid', 'issuanceLocation': 'Madrid', 'issuanceDate': '2015-04-14', 'number': '00000000', 'expiryDate': '2025-04-14', 'issuanceCountry': 'ES', 'validityCountry': 'ES', 'nationality': 'ES', 'holder': True } ] }
 
 try:
     # Flight Offers Search to search for flights from MAD to ATH
