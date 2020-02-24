0ea9ed3b69e0acb4881ea9a74edccef510a660b2
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-02-24 12:18:51 +0100

    remove support for most searched destinations

--- amadeus/travel/analytics/_air_traffic.py
+++ amadeus/travel/analytics/_air_traffic.py
@@ -1,8 +1,6 @@
 from amadeus.client.decorator import Decorator
 from .air_traffic._traveled import Traveled
 from .air_traffic._booked import Booked
-from .air_traffic._searched import Searched
-from .air_traffic._searched_by_destination import SearchedByDestination
 from .air_traffic._busiest_period import BusiestPeriod
 
 
@@ -11,6 +9,4 @@ class AirTraffic(Decorator, object):
         Decorator.__init__(self, client)
         self.booked = Booked(client)
         self.traveled = Traveled(client)
-        self.searched = Searched(client)
-        self.searched_by_destination = SearchedByDestination(client)
         self.busiest_period = BusiestPeriod(client)
