deb3eb1670a67e1331fa430872c0271ce5ff478b
Anna Tsolakou <tsolakou.a@gmail.com>
2020-04-24 16:33:04 +0200

    Trip Parser Examples  (#86)
    * add 2 examples for trip parser with base64 and file
    
    * merge to master and change sleep time

---
+++ examples/trip_parser/from_file/trip_parser.py
@@ -0,0 +1,33 @@
+import time
+from amadeus import Client, ResponseError
+
+amadeus = Client()
+
+booking_file = 'sample.eml'
+
+# Starts the parsing process
+try:
+    response = amadeus.travel.trip_parser_jobs.post(
+        amadeus.travel.from_file(booking_file))
+    id = response.data['id']
+except ResponseError as error:
+    raise error
+
+
+# Retrieves the status of the process
+def get_status(id):
+    try:
+        response = amadeus.travel.trip_parser_jobs.status(id).get()
+    except ResponseError as error:
+        raise error
+    return response.data['status']
+
+
+# Retrieves the parsing result when the process is complete
+while get_status(id) != 'COMPLETED':
+    time.sleep(10)
+try:
+    result = amadeus.travel.trip_parser_jobs.result(id).get()
+except ResponseError as error:
+    raise error
+# print(result.data)
