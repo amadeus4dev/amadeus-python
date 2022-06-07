98884c03d7342bb69ccfce7e8a216dbde90fb669
Anna Tsolakou <anna.tsolakou@amadeus.com>
2022-06-07 17:59:52 +0200

    city search

--- amadeus/reference_data/locations/__init__.py
+++ amadeus/reference_data/locations/__init__.py
@@ -1,5 +1,6 @@
 from ._airports import Airports
 from ._points_of_interest import PointsOfInterest
 from ._hotel import Hotel
+from ._cities import Cities
 
-__all__ = ['Airports', 'PointsOfInterest', 'Hotel']
+__all__ = ['Airports', 'PointsOfInterest', 'Hotel', 'Cities']
