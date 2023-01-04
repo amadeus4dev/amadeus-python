fb54f43d9ed5b9b97c5ca4ea0e9e9cdf55e959a2
Anna Tsolakou <anna.tsolakou@amadeus.com>
2023-01-04 18:09:47 +0100

    update version

--- specs/version_spec.py
+++ specs/version_spec.py
@@ -1,8 +1,5 @@
-from mamba import description, it
-from expects import expect, be_none
-
 from amadeus import version
 
-with description('Amadeus') as self:
-    with it('should have a version'):
-        expect(version).not_to(be_none)
+
+def test_version():
+    assert version is not None
