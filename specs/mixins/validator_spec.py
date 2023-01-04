e41180d994aebc799aac0f7f0bb8413ebe47a030
Anna Tsolakou <anna.tsolakou@amadeus.com>
2023-01-04 16:14:55 +0100

    update validator

--- specs/mixins/validator_spec.py
+++ specs/mixins/validator_spec.py
@@ -1,86 +1,89 @@
-from mamba import description, context, it, before
-from expects import expect, be_a, raise_error, equal, be
-from doublex_expects import have_been_called_with
-from doublex import Spy
+
 
 from amadeus import Client
 from os import environ
 from logging import Logger, getLogger
+import pytest
+from mock import MagicMock
 
-with description('Mixins/Validator') as self:
-    with before.all:
-        self.valid_params = {
-            'client_id': '1234',
-            'client_secret': '4546'
-        }
 
-    with context('Client.__init__'):
-        with it('should create a new client with direct variables'):
-            client = Client(**self.valid_params)
-            expect(client).to(be_a(Client))
+@pytest.fixture
+def valid_params():
+    return {
+        'client_id': '1234',
+        'client_secret': '4546'
+    }
 
-        with it('should create a new client with environment variables'):
-            environ['AMADEUS_CLIENT_ID'] = '123'
-            environ['AMADEUS_CLIENT_SECRET'] = '234'
 
-            expect(Client()).to(be_a(Client))
+def test_client_init(valid_params):
+    client = Client(**valid_params)
+    assert isinstance(client, Client)
 
-            del environ['AMADEUS_CLIENT_ID']
-            del environ['AMADEUS_CLIENT_SECRET']
 
-        with context('with missing parameters'):
-            with it('should refuse to create a new client without client_id'):
-                del self.valid_params['client_id']
-                expect(lambda: Client(**self.valid_params)).to(
-                    raise_error(ValueError)
-                )
+def test_client_init_with_env_vars(valid_params):
+    environ['AMADEUS_CLIENT_ID'] = '123'
+    environ['AMADEUS_CLIENT_SECRET'] = '234'
 
-            with it('should refuse to create a new client w/o client_secret'):
-                del self.valid_params['client_secret']
-                expect(lambda: Client(**self.valid_params)).to(
-                    raise_error(ValueError)
-                )
+    client = Client()
+    assert isinstance(client, Client)
 
-        with it('should by default have a logger'):
-            amadeus = Client(**self.valid_params)
-            expect(amadeus.logger).to(be_a(Logger))
-            expect(amadeus.log_level).to(equal('silent'))
+    del environ['AMADEUS_CLIENT_ID']
+    del environ['AMADEUS_CLIENT_SECRET']
 
-        with it('should allow for setting a custom logger'):
-            logger = getLogger('amadeus')
-            self.valid_params['logger'] = logger
-            amadeus = Client(**self.valid_params)
-            expect(amadeus.logger).to(be(logger))
-            expect(amadeus.log_level).to(equal('silent'))
 
-        with it('should persist a custom log level'):
-            logger = getLogger('amadeus')
-            logger.setLevel(10)
-            self.valid_params['logger'] = logger
-            amadeus = Client(**self.valid_params)
-            expect(amadeus.logger).to(be(logger))
-            expect(amadeus.logger.level).to(be(10))
+def test_client_init_with_missing_params(valid_params):
+    # Test missing client_id
+    valid_params_copy = valid_params.copy()
+    del valid_params_copy['client_id']
+    with pytest.raises(ValueError):
+        Client(**valid_params_copy)
 
-        with it('should warn when an unrecognized option is passed in'):
-            logger = Spy()
-            self.valid_params['logger'] = logger
-            self.valid_params['foobar'] = 'test'
-            Client(**self.valid_params)
-            expect(logger.warning).to(
-                have_been_called_with('Unrecognized option: foobar')
-            )
+    # Test missing client_secret
+    valid_params_copy = valid_params.copy()
+    del valid_params_copy['client_secret']
+    with pytest.raises(ValueError):
+        Client(**valid_params_copy)
 
-        with it('should default to the test host'):
-            amadeus = Client(**self.valid_params)
-            expect(amadeus.host).to(equal(Client.HOSTS['test']))
 
-        with it('should allow for setting a different hostname'):
-            self.valid_params['hostname'] = 'production'
-            amadeus = Client(**self.valid_params)
-            expect(amadeus.host).to(equal(Client.HOSTS['production']))
+def test_client_logging(valid_params):
+    # Test default logger
+    amadeus = Client(**valid_params)
+    assert isinstance(amadeus.logger, Logger)
+    assert amadeus.log_level == 'silent'
 
-        with it('should allow for setting a full different host'):
-            host = 'http://foo.bar.com/'
-            self.valid_params['host'] = host
-            amadeus = Client(**self.valid_params)
-            expect(amadeus.host).to(equal(host))
+    # Test custom logger
+    logger = getLogger('amadeus')
+    valid_params['logger'] = logger
+    amadeus = Client(**valid_params)
+    assert amadeus.logger is logger
+    assert amadeus.log_level == 'silent'
+
+    # Test custom log level
+    logger.setLevel(10)
+    amadeus = Client(**valid_params)
+    assert amadeus.logger is logger
+    assert amadeus.logger.level == 10
+
+
+def test_client_options(valid_params):
+    # Test unrecognized option warning
+    logger = MagicMock()
+    valid_params['logger'] = logger
+    valid_params['foobar'] = 'test'
+    Client(**valid_params)
+    logger.warning.assert_called_with('Unrecognized option: foobar')
+
+    # Test default host
+    amadeus = Client(**valid_params)
+    assert amadeus.host == Client.HOSTS['test']
+
+    # Test custom hostname
+    valid_params['hostname'] = 'production'
+    amadeus = Client(**valid_params)
+    assert amadeus.host == Client.HOSTS['production']
+
+    # Test custom host
+    host = 'http://foo.bar.com/'
+    valid_params['host'] = host
+    amadeus = Client(**valid_params)
+    assert amadeus.host == host
