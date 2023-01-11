1d0d81e706fa7bb79aa9d5fbc88e22351086538b
Anna Tsolakou <anna.tsolakou@amadeus.com>
2023-01-11 09:58:21 +0100

    test hotel subtype

--- specs/test_client.py
+++ specs/test_client.py
@@ -1,4 +1,4 @@
-from amadeus import Client, Location
+from amadeus import Client, Location, Hotel
 
 
 def test_client_exists():
@@ -8,3 +8,9 @@ def test_client_exists():
 def test_client_has_helper_locations():
     assert Location is not None
     assert Location.ANY == 'AIRPORT,CITY'
+
+
+def test_client_has_helper_hotels():
+    assert Hotel is not None
+    assert Hotel.HOTEL_GDS == 'HOTEL_GDS'
+    assert Hotel.HOTEL_LEISURE == 'HOTEL_LEISURE'
